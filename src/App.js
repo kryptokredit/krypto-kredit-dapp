184f7bfedc4075a2732abe00fb7bc3dc15be543c
Luis C Mogrovejo <luis.c.mogrovejo@gmail.com>
2023-03-01 23:37:29 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Routes, Route} from "react-router-dom";
 import Home from "./Home";
 import MyInvoices from "./InvoiceList";
 import CreateInvoice from "./invoiceForm";
+import InvoiceList from "./PayerInvoiceList"
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route exact path="/myInvoices" element={MyInvoices} />
-        <Route exact path="/createInvoice" element={CreateInvoice} />
+        <Route exact path="/myInvoices" element={<MyInvoices />} />
+        <Route exact path="/createInvoice" element={<CreateInvoice />} />
+        <Route exact path="/payerList" element={<InvoiceList />} />
       </Routes>
     </div>
   );
