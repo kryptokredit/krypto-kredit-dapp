14b2f287283635a1393261a65c5655ae6d49fefa
Luis C Mogrovejo <101224235+Luiscmogrovejo@users.noreply.github.com>
2023-03-02 18:49:20 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,39 @@
 import { Navbar, Nav, Dropdown } from "react-bootstrap";
+import {
+  useDynamicContext,
+} from "@dynamic-labs/sdk-react";
+import React from "react";
 
 function Header() {
+
+  const {
+    user,
+    isAuthenticated,
+    primaryWallet,
+    handleLogOut,
+    connectedWallets,setShowAuthFlow,
+    showAuthFlow,
+    walletConnector,
+  } = useDynamicContext();
+  const [balance, setBalance] = React.useState(null);
+  const [addressWallet, setAddressWallet] = React.useState("");
+
+  async function handleWallet() {
+    setAddressWallet(await walletConnector.fetchPublicAddress());
+  }
+
+  React.useEffect(() => {
+    if (user && walletConnector) {
+      const provider = walletConnector.getWeb3Provider();
+      handleWallet();
+      provider
+        .getBalance(user.walletPublicKey)
+        .then((balance) => {
+          setBalance(balance.toString());
+        });
+    }
+  }, [user, walletConnector]);
+
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="/">
@@ -16,10 +49,8 @@ function Header() {
           <Dropdown as={Nav.Item}>
             <Dropdown.Toggle as={Nav.Link}>Invoicer</Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item href="/InvoiceList">My Invoices</Dropdown.Item>
-              <Dropdown.Item href="/InvoiceForm">
-                Create an Invoice
-              </Dropdown.Item>
+              <Dropdown.Item href="/InvoiceForm">My Invoices</Dropdown.Item>
+              <Dropdown.Item href="#">Create an Invoice</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
           <Dropdown as={Nav.Item}>
@@ -33,10 +64,27 @@ function Header() {
           </Dropdown>
           <Nav.Item>
             <button
-              style={{ marginRight: "15px", backgroundColor: "#12E26C" }}
-              className="btn"
+              style={{
+                fontSize: "12px" /* adjust the font size as needed */,
+                fontWeight: "bold",
+                color:
+                  "#fff" /* set the font color to contrast with the background color */,
+                backgroundColor: "#12E26C",
+                border: "none",
+                borderRadius: "4px",
+                padding: "10px 20px",
+                transition: "all 0.3s ease-in-out",
+                marginRight: "15px",
+              }}
+              onClick={() => {
+                console.log("isAuthenticated", isAuthenticated);
+                console.log("addressWallet", addressWallet);
+                setShowAuthFlow(true);
+              }}
             >
-              Connect
+              {isAuthenticated
+                ? addressWallet.slice(0, 6) +"..." + addressWallet.slice(39, 42)
+                : "Connect"}
             </button>
           </Nav.Item>
         </Nav>
